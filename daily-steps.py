439266182abcc770e97055da66adbbcd3e982532
Atharv Sharma <atharv@Atharvs-MacBook-Pro.local>
2025-02-22 13:14:30 +0530

    Update the number of days for steps data

--- daily-steps.py
+++ daily-steps.py
@@ -9,7 +9,7 @@ def get_all_daily_steps(garmin):
     Get last x days of daily step count data from Garmin Connect.
     """
     startdate = date.today() - timedelta(days=1)
-    daterange = [startdate + timedelta(days=365) 
+    daterange = [startdate + timedelta(days=x) 
                  for x in range((date.today() - startdate).days)] # excl. today
     daily_steps = []
     for d in daterange:
